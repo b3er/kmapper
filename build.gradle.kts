658d6265031ff6842ae9c55a7cb74493bad80dbb
Ilya Usanov <b3er@who.ms>
2023-05-04 10:58:21 +0300

    Update kotlin and ksp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 subprojects {
     group = "com.github.b3er.kmapper"
-    version = "0.5.3"
+    version = "0.5.4"
     plugins.withId("maven-publish") {
         publishing {
             publications {
